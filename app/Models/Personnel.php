6cec141c85d08af7cac0bcf321db7c7b6040d4dc
SamuraiMe <samurai7@foxmail.com>
2015-12-28 00:42:15 +0800

    amaze竟然也会有泥潭让人踩

--- app/Models/Personnel.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Personnel extends Model
-{
-    protected $table = 'personnel';
-    protected $guarded = ['id', 'created_at', 'updated_at'];
-}
